5f6c4e4523932a8fafac9bf6de3af780f807cdb1
Muhammad Asfa Devano <masfadevano72@gmail.com>
2025-08-01 22:56:22 +0700

    mengupdate sintaks yang typo

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     imageUrl: { type: String, required: true },
-    cartItems: { type: object, default: {} },
+    cartItems: { type:  mongoose.Schema.Types.Mixed, default: {} },
 }, { minimize: false })
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
